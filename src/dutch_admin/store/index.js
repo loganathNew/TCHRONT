717c0ea152e1a672fd1d363959f25c1ffdddcc69
loganathNew <loguphp2020@gmail.com>
2023-05-23 11:11:55 +0530

    store-index-changed

--- src/dutch_admin/store/index.js
+++ src/dutch_admin/store/index.js
@@ -5,7 +5,7 @@ let token = window.localStorage.getItem("token");
 let user_name = window.localStorage.getItem("user_name");
 let user_role = window.localStorage.getItem("user_role");
 let user_id = window.localStorage.getItem("user_id");
-let permissions = JSON.parse(window.localStorage.getItem("permissions"));
+let permissions = (window.localStorage.getItem("permissions") != "" && window.localStorage.getItem("permissions").length != 0) ? JSON.parse(window.localStorage.getItem("permissions")) : [];
 
 let state = {
     hamActive: true,
